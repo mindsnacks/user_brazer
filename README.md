f7340c187e30bc27edc3beefdb3986a3b97df28a
jgerminario <jgerminario@gmail.com>
2018-02-14 15:51:17 -0800

    : adds attribute exporter

---
+++ README.md
@@ -0,0 +1,14 @@
+## Set up
+
+- Clone somewhere on your computer (`git clone git@github.com:mindsnacks/user_brazer.git`)
+- Run `gem install httparty`
+
+## Running a file
+
+- Set up your explore. You will need to have a column for Users -> Elevate User ID and a column for whatever attribute you want to export. Here's an example: https://elevatelabs.looker.com/explore/elevate_datawarehouse/users?qid=5dyJ80uCaAY3u39ySXjCHl
+- Go to the gear next to the Run button, then Download. Make sure to download All Results (in the Limit section).
+- Go into the downloaded file and remove the first column of row numbers
+- The new first column should have the header 'user_id'
+- The second column should have the header 'looker_export.<NAME_OF_YOUR_ATTRIBUTE>' where <NAME_OF_YOUR_ATTRIBUTE> is the name you want for the new attribute.
+- Run the file by running `ruby attribute_exporter.rb` in the user_brazer folder
+- QA by taking various user IDs from the CSV and checking if the attribute shows up on the user records. If this is your first time running this, try doing a test run by using a dummy attribute (e.g. `looker_export.test_on_2018_02_09`)—we can always blacklist old/test attributes, so it's better to try it out first instead of potentially messing up the attribute you want to target.
---
+++ attribute_exporter.rb
@@ -0,0 +1,98 @@
+# gem install httparty
+
+require 'httparty'
+require 'uri'
+require 'csv'
+
+APPBOY_URL = 'https://api.appboy.com/users/track'
+FILE_NAME = 'user_ids_to_export.csv'
+VALID_ATTRIBUTE_PREFIX = 'looker_export.'
+
+class AttributeExporter
+  def initialize
+    full_array = CSV.read(FILE_NAME, "r:ISO-8859-1")
+
+    @array_count = full_array.length
+    @array_of_ids = full_array.each_slice(50).to_a
+
+    header_row = @array_of_ids[0].shift
+    @attribute_name = header_row[1]
+    @user_id_column_name = header_row[0]
+  end
+
+  def send_if_confirmed
+    if is_valid_user_id_name
+      if is_valid_attribute_name
+        if is_confirmed
+          batch_send_attributes
+        else
+          puts "Attribute export cancelled."
+        end
+      else
+        puts "Second CSV column (attribute name) must start with 'users.' Attribute name given: #{@attribute_name}"
+      end
+    else
+      puts "First CSV column (user ID) must be named 'user_id'"
+    end
+  end
+
+  private
+
+  def is_valid_user_id_name
+    @user_id_column_name == 'user_id'
+  end
+
+  def is_valid_attribute_name
+    @attribute_name.start_with?(VALID_ATTRIBUTE_PREFIX)
+  end
+
+  def is_confirmed
+    puts "Import #{@array_count} records to attribute name '#{@attribute_name}'? (y/n)"
+    return gets.chomp.downcase == 'y'
+  end
+
+  def batch_send_attributes
+    count = 0
+    @array_of_ids.each do |array_batch|
+      array_of_attributes = []
+      array_batch.each do |user_item|
+        count += 1
+        array_of_attributes << user_attributes_object(user_item)
+      end
+      attribute_payload = payload(array_of_attributes)
+      response = call_appboy(attribute_payload)
+      if response.code == 201
+        puts("#{count} out of #{@array_count} users processed")
+      else
+        puts("Error:")
+        puts(response)
+      end
+    end
+  end
+
+  def call_appboy(attribute_payload)
+    HTTParty.post(APPBOY_URL, body: attribute_payload.to_json, headers: headers)
+  end
+
+  def payload(array_of_attributes)
+    {
+      "app_group_id" => ENV['APPBOY_APP_GROUP_ID_PRODUCTION'],
+      "attributes" => array_of_attributes
+    }
+  end
+
+  def user_attributes_object(user_item)
+    user_id = user_item[0]
+    attribute_value = user_item[1]
+    {
+      "external_id" => user_id,
+      @attribute_name => attribute_value
+    }
+  end
+
+  def headers
+    { 'Content-Type' => 'application/json' }
+  end
+end
+
+AttributeExporter.new.send_if_confirmed
